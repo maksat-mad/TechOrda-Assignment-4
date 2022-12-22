322fe6bc130676e28a5f13f05584960d554fa49a
Maksat <maksat.madeniyetov@gmail.com>
2022-12-22 14:37:17 +0600

    optimation of calc

--- solutions/Day_9_Binary_Calculator/js/binaryCalculator.js
+++ solutions/Day_9_Binary_Calculator/js/binaryCalculator.js
@@ -1,46 +1,43 @@
 let lastExpr = "";
 const opers = ["+", "-", "*", "/", ""];
+let res = document.getElementById("res");
 
-function addZero() {
-    document.getElementById("res").innerHTML += "0";
-    lastExpr = "0";
-}
-
-function addOne() {
-    document.getElementById("res").innerHTML += "1";
-    lastExpr = "1";
-}
-
-function clearAll() {
-    document.getElementById("res").innerHTML = "";
-    lastExpr = "";
-}
-
-function addPlus() {
-    document.getElementById("res").innerHTML += opers.includes(lastExpr) ? "" : "+";
-    lastExpr = "+";
-}
-
-function addMinus() {
-    document.getElementById("res").innerHTML += opers.includes(lastExpr) ? "" : "-";
-    lastExpr = "-";
-}
-
-function addMul() {
-    document.getElementById("res").innerHTML += opers.includes(lastExpr) ? "" : "*";
-    lastExpr = "*";
-}
-
-function addDiv() {
-    document.getElementById("res").innerHTML += opers.includes(lastExpr) ? "" : "/";
-    lastExpr = "/";
-}
-
-function calc() {
-    if (opers.includes(lastExpr) || !document.getElementById("res").innerHTML.match(/[+|\-|*|\/]+/g)) {
-        return;
+function calc(value) {
+    switch (value) {
+        case '0':
+            res.innerHTML += "0";
+            lastExpr = "0";
+            break;
+        case '1':
+            res.innerHTML += "1";
+            lastExpr = "1";
+            break;
+        case 'c':
+            res.innerHTML = "";
+            lastExpr = "";
+            break;
+        case '+':
+            res.innerHTML += opers.includes(lastExpr) ? "" : "+";
+            lastExpr = "+";
+            break;
+        case '-':
+            res.innerHTML += opers.includes(lastExpr) ? "" : "-";
+            lastExpr = "-";
+            break;
+        case '*':
+            res.innerHTML += opers.includes(lastExpr) ? "" : "*";
+            lastExpr = "*";
+            break;
+        case '/':
+            res.innerHTML += opers.includes(lastExpr) ? "" : "/";
+            lastExpr = "/";
+            break;
+        case 'o':
+            if (opers.includes(lastExpr) || !res.innerHTML.match(/[+|\-|*|\/]+/g)) {
+                return;
+            }
+            let expr = res.innerHTML;
+            expr = expr.replace(/(\d+)/g, el => parseInt(el, 2));
+            res.innerHTML = eval(expr).toString(2);
     }
-    let expr = document.getElementById("res").innerHTML;
-    expr = expr.replace(/(\d+)/g, el => parseInt(el, 2));
-    document.getElementById("res").innerHTML = eval(expr).toString(2);
 }
